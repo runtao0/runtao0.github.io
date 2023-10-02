c11c1875bc349efa9329964402a3ffc705bcaad0
Runtao Yang <runtaoyang@runtaos-mbp.myfiosgateway.com>
2023-10-01 22:47:23 -0400

    changes to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,26 @@
+const path = require('path');
+
 module.exports = {
   entry: './lib/main.js',
   output: {
-    path: './lib',
+    path: path.resolve(__dirname, 'lib'),
     filename: 'bundle.js',
   },
-  module: {
-    loaders: [
-      {
-        test: [/\.js?$/],
-        exclude: /(node_modules)/,
-        loader: 'babel',
-        query: {
-          presets: ['es2015']
-        }
-      }
-    ]
-  },
-  devtool: 'source-map',
-  resolve: {
-    extensions: ['', '.js']
-  },
-  debug: true,
+  // module: {
+  //   loaders: [
+  //     {
+  //       test: [/\.js?$/],
+  //       exclude: /(node_modules)/,
+  //       loader: 'babel',
+  //       query: {
+  //         presets: ['es2015']
+  //       }
+  //     }
+  //   ]
+  // },
+  // devtool: 'source-map',
+  // resolve: {
+  //   extensions: ['', '.js']
+  // },
+  // debug: true,
 };
